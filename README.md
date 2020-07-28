a3f28cfb1fb58b0ea6217a45f0f7bee4254a8375
Steven Davidovitz <steven.davidovitz@dominodatalab.com>
2020-07-28 12:30:50 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## container-runtime-interface-api
+
+Python library for communication with the Kubernetes [Container Runtime Interface API](https://github.com/kubernetes/cri-api).
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="container-runtime-interface-api",
+    packages=["container-runtime-interface-api"],
+    package_dir={"": "src"},
+    version="0.0.1",
+    author="Domino Data Lab",
+    author_email="steven.davidovitz@dominodatalab.com",
+    description="Python library for interaction with the Kubernetes container runtime interface API.",  # noqa
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dominodatalab/container-runtime-interface-api",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
