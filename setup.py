33155e39382ee50e0f61949f7319508aa86e6592
Steven Davidovitz <steven.davidovitz@dominodatalab.com>
2021-09-02 19:43:41 -0700

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     packages=setuptools.find_namespace_packages(where="src"),
     package_dir={"": "src"},
     install_requires=["grpcio~=1.39.0", "protobuf~=3.17.3"],
-    version="0.0.5",
+    version="0.1.0",
     author="Domino Data Lab",
     author_email="steven.davidovitz@dominodatalab.com",
     description="Python library for interaction with the Kubernetes container runtime interface API.",  # noqa
