6a88f85f238d226e77e559aa894254ad9d6d00da
Dan Clegg <dan.clegg@dominodatalab.com>
2024-04-17 13:15:44 -0600

    Updates for post 1.26

--- bin/update-proto.sh
+++ bin/update-proto.sh
@@ -5,8 +5,7 @@ set -ex
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$SCRIPT_DIR/../" || exit 1
 
-# Commented out until 1.26 is fully supported
-# git submodule update --init --recursive --remote --merge
+git submodule update --init --recursive --remote --merge
 
 pipenv run python -m grpc_tools.protoc \
   -I vendor \
@@ -16,16 +15,6 @@ pipenv run python -m grpc_tools.protoc \
   --mypy_out=src/cri_api \
   --grpc_python_out=src/cri_api \
   v1/api.proto \
-  vendor/github.com/gogo/protobuf/gogoproto/gogo.proto
-
-pipenv run python -m grpc_tools.protoc \
-  -I vendor \
-  -I vendor/github.com/gogo/protobuf/ \
-  -I vendor/cri-api/pkg/apis/runtime/ \
-  --python_out=src/cri_api \
-  --mypy_out=src/cri_api \
-  --grpc_python_out=src/cri_api \
-  v1alpha2/api.proto \
   vendor/github.com/gogo/protobuf/gogoproto/gogo.proto
 
 find src/cri_api/ -type f -name '*.py' -print0 | xargs -0 -P 1 -I {} sed -i '' -Ee 's/from (v1|github)/from cri_api.\1/' {}
